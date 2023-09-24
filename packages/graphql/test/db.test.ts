6ec1b87e07d8425aab185404daa4b25819a1a89e
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-09-24 15:16:09 -0500

    test: Add test Database (#7)

--- packages/graphql/test/db.test.ts
+++ packages/graphql/test/db.test.ts
@@ -1,3 +1,16 @@
-test("should return true", () => {
-    expect()
+import { UTIL } from "src/const";
+import Database from "src/db";
+
+
+let database: Database;
+
+beforeAll(() => {
+  database = new Database(UTIL.__MONGODB_URI__);
 })
+
+test("should return true", async () => {
+
+  const connection = await database.conn()
+
+  expect(connection).toBeDefined()
+});
